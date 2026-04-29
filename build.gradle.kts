fd1170c4056ce4ad382c89ae7fd3f6d51118a3bb
Harshit Gulati <harshit14012018@gmail.com>
2026-04-29 20:25:08 +0530

    cleanup: remove root files and submodule

--- build.gradle.kts
+++
@@ -1,6 +0,0 @@
-// Top-level build file
-plugins {
-    id("com.android.application") version "8.12.3" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.21" apply false
-    id("com.google.dagger.hilt.android") version "2.48" apply false
-}
